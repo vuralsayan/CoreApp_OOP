0b90b7760562f9f7150d6b54aa5e82707f452068
Vural Sayan <vuralsayan01@gmail.com>
2024-01-04 21:29:38 +0300

    Context sınıfı düzenlendi

---
+++ CoreApp_OOP/ProjeContext/Context.cs
@@ -0,0 +1,18 @@
+﻿using CoreApp_OOP.Entity;
+using Microsoft.EntityFrameworkCore;
+
+namespace CoreApp_OOP.ProjeContext
+{
+    public class Context : DbContext
+    {
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlServer(@"Server=Vural\SQLEXPRESS;Database=DbNewOOPCore;integrated security=true;");
+        }
+
+        public DbSet<Product> Products { get; set; }
+        public DbSet<Customer> Customers { get; set; }
+        public DbSet<Category> Categories { get; set; }
+
+    }
+}
